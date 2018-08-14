aef0e83e72d7bf768a23c0becf11235b18155b6f
mikespriggs26 <mikespriggs@msn.com>
2018-08-13 21:18:58 -0400

    Added updates

--- HTML-API-Proj/scripts.js
+++ HTML-API-Proj/scripts.js
@@ -1,7 +1,7 @@
 ﻿const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-logo.src = 'logo.png';
+//logo.src = 'logo.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
@@ -10,27 +10,17 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://api.iextrading.com/1.0/stock/aapl/company', true);
+
+//request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
 
     // Begin accessing JSON data here
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
-
-            const h1 = document.createElement('h1');
-            h1.textContent = movie.title;
-
-            const p = document.createElement('p');
-            movie.description = movie.description.substring(0, 300);
-            p.textContent = `${movie.description}...`;
-
-            container.appendChild(card);
-            card.appendChild(h1);
-            card.appendChild(p);
-        });
+        
+        document.write(data.symbol);
+        ;
     } else {
         const errorMessage = document.createElement('marquee');
         errorMessage.textContent = `Gah, it's not working!`;
