dd4b4efc7c1e050e239c3c9b002d3143646b4137
mikespriggs26 <mikespriggs@msn.com>
2018-08-28 20:20:59 -0400

    updates

--- HTML-API-Proj/scripts.js
+++ HTML-API-Proj/scripts.js
@@ -12,13 +12,15 @@ function myFunction() {
     var symbol = document.getElementById('id1').value;
     //var API_Path = "https://api.iextrading.com/1.0/stock/{0}/company";
     //API_Path = string.Format(API_Path, symbol);
+    //var url = "https://api.iextrading.com/1.0/stock/aapl/company";
+    var url = "\"https://api.iextrading.com/1.0/stock/${symbol}/company \"";
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("demo").innerHTML = this.responseText;
         
         }
     };
-    xhttp.open("GET", "https://api.iextrading.com/1.0/stock/aapl/company", true);
+    xhttp.open("GET", url, true);
     xhttp.send();
 }
 
